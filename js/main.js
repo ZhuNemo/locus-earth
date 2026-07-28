177a8f33ef815826a94840f97349a4843f9e7c04
Nemo <ac-honor@outlook.com>
2026-07-28 11:41:51 +0800

    Update Ion.defaultAccessToken value

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ import { initViewer } from './viewer.js';
 
 import { initUI } from './ui.js';
 
-        Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJjMjgxOGI5ZS1lYzQ1LTRkN2ItYWRmYy05YzllOTNhYWZjYzQiLCJpZCI6NDUyMzc2LCJzdWIiOiJaaHVOZW1vIiwiaXNzIjoiaHR0cHM6Ly9hcGkuY2VzaXVtLmNvbSIsImF1ZCI6IlpodU5lbW9fZGVmYXVsdCIsImlhdCI6MTc4MzIyMzUwOX0.HdLWoiGJw7McbyHwjra0Bx7J57pVrZGIJfNk0AZjQBU';
+        Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI3MGY5OGU2Yy1lZTcxLTRhNDUtOTJlNC1hZjNkNzQ3M2VlZGEiLCJpZCI6NDUyMzc2LCJzdWIiOiJaaHVOZW1vIiwiaXNzIjoiaHR0cHM6Ly9hcGkuY2VzaXVtLmNvbSIsImF1ZCI6IkxvY3VzIE1hcHMiLCJpYXQiOjE3ODUwNDE3NTB9.AS3v82gVfiQzkfgaJ-GVK_ly4RymaRyaJ9f9ol94AuU';
 
         const terrainProvider = await CesiumTerrainProvider.fromIonAssetId(1, {
             requestVertexNormals: true,
